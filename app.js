38a565084411985d97ddb7cab558dc37d40cef3b
Pablo Fiorido <pablofiorido@hotmail.com>
2020-06-10 16:55:37 -0300

    clase 03: app js onclick prop added

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log('test')
+var constructor = []
+var column1
+var column2
+var column3
+var column4
+var column5
+var column6
+var column7
+
+function clicked() {
+    for (var i = 0; i < constructor.length; i++) {
+    }
+}
+
+function control() {
+    console.log("clicked")
+}
+
+window.onload = function () {
+
+    constructor = document.getElementsByClassName("column")
+
+    column1 = document.getElementById("column1")
+    column2 = document.getElementById("column2")
+    column3 = document.getElementById("column3")
+    column4 = document.getElementById("column4")
+    column5 = document.getElementById("column5")
+    column6 = document.getElementById("column6")
+    column7 = document.getElementById("column7")
+
+    column1.onclick = control()
+
+    console.log(constructor)
+}
