a25384f4d9d5adb555b35851f23c726d1bc4a585
Pablo Fiorido <pablofiorido@hotmail.com>
2020-06-10 18:45:28 -0300

    class 3: cell painting added to js file

--- app.js
+++ app.js
@@ -7,19 +7,66 @@ var column5
 var column6
 var column7
 
-function clicked() {
-    for (var i = 0; i < constructor.length; i++) {
-    }
+var col1cell7
+var col2cell7
+var col3cell7
+var col4cell7
+var col5cell7
+var col6cell7
+var col7cell7
+
+//function clicked() {
+//    for (var i = 0; i < constructor.length; i++) {
+//    }
+//}
+
+
+function control1() {
+    console.log("click ok test")
+    col1cell7 = document.getElementById("column1-c7")
+    col1cell7.style.background = 'red'
 }
 
-function control() {
-    console.log("clicked")
+function control2() {
+
+    col2cell7 = document.getElementById("column2-c7")
+    col2cell7.style.background = 'red'
 }
+
+function control3() {
+
+    col3cell7 = document.getElementById("column3-c7")
+    col3cell7.style.background = 'red'
+}
+
+function control4() {
+
+    col4cell7 = document.getElementById("column4-c7")
+    col4cell7.style.background = 'red'
+}
+
+function control5() {
+
+    col5cell7 = document.getElementById("column5-c7")
+    col5cell7.style.background = 'red'
+}
+
+function control6() {
+
+    col6cell7 = document.getElementById("column6-c7")
+    col6cell7.style.background = 'red'
+}
+
+function control7() {
+
+    col7cell7 = document.getElementById("column7-c7")
+    col7cell7.style.background = 'red'
+}
+
 
 window.onload = function () {
 
-    constructor = document.getElementsByClassName("column")
-
+    //constructor = document.getElementsByClassName("column")
     column1 = document.getElementById("column1")
     column2 = document.getElementById("column2")
     column3 = document.getElementById("column3")
@@ -28,7 +75,13 @@ window.onload = function () {
     column6 = document.getElementById("column6")
     column7 = document.getElementById("column7")
 
-    column1.onclick = control()
+    column1.onclick = control1
+    column2.onclick = control2
+    column3.onclick = control3
+    column4.onclick = control4
+    column5.onclick = control5
+    column6.onclick = control6
+    column7.onclick = control7
 
-    console.log(constructor)
+    //console.log(constructor)
 }
